27b2ba1455c1742b1bd9d60f3563ca9fb28141ad
MyDearest <dearestestelle@gmail.com>
2020-01-03 16:34:34 +0800

    基本完善使用json包装传输任务包的方法send_task_with_json()，同时将数据包发送的部分改为单独的函数send_data()进行控制，下一步将这些打包成类

--- modules/transmit/transfer.py
+++ modules/transmit/transfer.py
@@ -17,22 +17,65 @@ def getChecksum(bstream):
 MAX_CLINET_ID = 32
 MAX_IMG_FILENAME = 32
 MAX_RECV = 1024
-MAX_TIMEOUT = 15 # 这个时间还要调，除非发送端拆成多个小包（比如一个包4KB)多次发送，不然都是头疼的事
+MAX_TIMEOUT = 5 # 这个时间还要调，除非发送端拆成多个小包（比如一个包4KB)多次发送，不然都是头疼的事
 MAX_RETRANS = 5
+MAX_SEG_SIZE = 4096 # 最大单次传输片段大小
 
-STAT_PROCESSING = 1
-STAT_RECV_COMPLETE = 2
-
+STAT_KEYBORDINTERRUPT = -3
+STAT_TIMEOUT = -2
+STAT_FAILURE = -1
+STAT_NOEEROR = 0
 trans_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = "198.13.44.143"
+host = "192.168.109.131"
 port = 9999
 
 trans_socket.bind((host, port))
 trans_socket.listen(5)
 
 
-img_filenames = ["ISIC_0012086.jpg", "ISIC_0012092.jpg", "ISIC_0012095.jpg"]
-# img_filenames = ["pixiv57735171_12.jpg", "pixiv57735171_11.jpg", "pixiv57735171_6.jpg"]
+# img_filenames = ["ISIC_0012086.jpg", "ISIC_0012092.jpg", "ISIC_0012095.jpg"]
+img_filenames = ["pixiv57735171_12.jpg", "pixiv57735171_11.jpg", "pixiv57735171_6.jpg"]
+
+def send_data(send_socket, bstream, max_seg_size, timeout, max_retry):
+
+    stat_flag = None
+
+    pre_timeout = send_socket.gettimeout()
+    # pre_timeout = None
+    send_socket.settimeout(timeout)
+
+    sent_size = 0
+    data_size = len(bstream)
+    
+    n_retry = 0
+    next_size = max_seg_size
+    while sent_size < data_size:
+        next_size = max_seg_size if data_size-sent_size>max_seg_size else data_size-sent_size 
+        try:
+            send_socket.sendall(bstream[sent_size:sent_size+next_size])
+            sent_size += next_size
+            print("Total Size: {}B | Sent: {}B".format(data_size, sent_size), end='\r')
+        except socket.timeout:
+            n_retry += 1
+            if n_retry >= max_retry:
+                print("Timeout Error")
+                stat_flag = STAT_TIMEOUT
+                break 
+        except KeyboardInterrupt:
+            print("Keyboard Interrupt")
+            stat_flag = STAT_KEYBORDINTERRUPT
+            break
+        except Exception as e:
+            print("Exception Catched: ",e)
+            stat_flag = STAT_FAILURE
+            break
+    send_socket.settimeout(pre_timeout)
+    stat_flag = STAT_NOEEROR
+    # print('\n')
+    if stat_flag == STAT_NOEEROR:
+        return sent_size
+    else:
+        return stat_flag
 
 def send_task():
     try:
@@ -78,7 +121,7 @@ def send_task():
 
 def send_task_with_json():
     task_socket, addr = trans_socket.accept()
-    task_socket.settimeout(MAX_TIMEOUT)
+    # task_socket.settimeout(MAX_TIMEOUT)
 
     client_id = "TEST"+str(int(time.time()))
     # client_id = client_id.encode('utf-8')
@@ -96,9 +139,9 @@ def send_task_with_json():
             img_file.close()
 
     bstream = json.dumps(task_pack).encode('utf-8')
-    size = len(bstream).to_bytes(4,"little")
-    # task_socket.send(size)
     cs = getChecksum(bstream).to_bytes(1,"little")
+    size = len(bstream).to_bytes(4,"little")
+    bstream = size+bstream+cs
     is_success = False
     n_timeout = 0
     print("Sending Task of {} | size: {}B".format(client_id, int.from_bytes(size,"little")))
@@ -107,24 +150,29 @@ def send_task_with_json():
             print("Connect Timeout")
             break
         try:
+            '''
             task_socket.sendall(size+bstream+cs)
-            # while True:
-            #     if task_socket.recv(1) == STAT_RECV_COMPLETE.to_bytes(1,"little"):
-            #         break
             task_socket.send(cs)
+            '''
+            sent_size = send_data(task_socket, bstream, MAX_SEG_SIZE, MAX_TIMEOUT, MAX_RETRANS)
+            print("Sent: {}B, wait for checking...".format(sent_size),end='\r')
+            
             if cs  == task_socket.recv(1):
                 is_success = True
+                print("Transmition of {} is successful".format(client_id))
                 task_socket.close()
             time.sleep(1)
-        # except socket.timeout:
-        #     print("Timeout")
-        #     n_timeout += 1
+        except socket.timeout:
+            print("Timeout")
+            n_timeout += 1
         except KeyboardInterrupt:
             task_socket.close()
             print("KeybordInterrupt")
         if is_success:
             break
-
+    # while not is_success:
+    #     sent_size = send_data(task_socket, bstream, MAX_SEG_SIZE, MAX_TIMEOUT)
+    #     print("Sent: {}B, wait for checking ... ".format(sent_size))
     pass
             
             
