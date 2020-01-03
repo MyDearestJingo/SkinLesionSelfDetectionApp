de33a1e063af2123d850982994fa1040c6fe3c37
MyDearest <dearestestelle@gmail.com>
2020-01-03 14:24:31 +0800

    主程序

---
+++ main.py
@@ -0,0 +1,11 @@
+'''
+主程序
+'''
+import sys
+sys.path.append("./modules")
+sys.path.append("./models")
+from InputData import InputData
+from Predictor import Predictor
+
+if __name__ == "__main__":
+    pass
