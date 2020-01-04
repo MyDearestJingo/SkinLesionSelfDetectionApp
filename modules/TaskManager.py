a9c8116dbb495930449d527fb55f74f7fb6e250d
MyDearest <dearestestelle@gmail.com>
2020-01-05 00:40:02 +0800

    数据发送与接收统一改为使用tools.py中的对应函数

--- modules/TaskManager.py
+++ modules/TaskManager.py
@@ -6,6 +6,7 @@ import base64
 import csv
 import os
 import json
+import tools
 
 MAX_CLINET_ID = 32
 MAX_IMG_FILENAME = 32
@@ -37,112 +38,55 @@ class TaskManager:
     def close(self):
         self.t_socket.close()
 
-    ''' def 从socket接收任务数据 '''
-    def recv_task(self):
-        self.t_socket.connect((self.host, self.port))
-
-        # 接收用户ID
-        client_id_len = int.from_bytes(self.t_socket.recv(1),byteorder="little")
-        client_id = self.t_socket.recv(client_id_len).decode("utf-8")
-        print("client_id: {}".format(client_id))
-        
-        # 创建任务目录
-        # os.mkdir(self.work_dir+"/"+client_id)
-        task_dir = os.path.join(self.work_dir,client_id)
-        os.mkdir(task_dir)
-        # task_dir = self.work_dir+client_id+"/"
-
-        # 接收本次任务所含图像数
-        n_img = int.from_bytes(self.t_socket.recv(1),byteorder="little")
-
-        img_list = []
-        for i in range(n_img): # todo: 加入异常处理以应对图像传输失败导致数量不符的情况
-            # 接收图像文件名
-            img_name_size = int.from_bytes(self.t_socket.recv(1),byteorder="little")
-            img_name = self.t_socket.recv(img_name_size).decode("utf-8")
-            
-            # 接收图像文件大小
-            img_size = int.from_bytes(self.t_socket.recv(4), byteorder="little")
-            # print("Client: {} | {} of {} | name: {} | size: {} Bytes".format(client_id, i+1, n_img, img_name, img_size))
-            
-            # 分段接收文件，每段大小不超过MAX_RECV字节
-            recv_size = 0
-            b_img = bytes()
-            recv_buff = bytes()
-            recv_success = False
-            while not recv_success:
-                while recv_size < img_size:
-                    next_size = MAX_RECV if img_size-recv_size > MAX_RECV else img_size-recv_size
-                    recv_buff = self.t_socket.recv(next_size)
-                    recv_size += len(recv_buff)
-                    b_img += recv_buff
-                    # t_socket.send(STAT_PROCESSING.to_bytes(1,"little"))
-                    print("Client: {} | {} of {} | name: {} | size: {}B | recv: {}B".format(client_id, i+1, n_img, img_name, img_size, recv_size),end='\r')
-                # t_socket.send(STAT_RECV_COMPLETE.to_bytes(1,"little"))
-                print("\n Trans Complete. Checking data ...",end='\r')
-                cs = self.get_checksum(b_img).to_bytes(1,"little")
-                # print("Recv size: {}".format(recv_size))
-                if cs != self.t_socket.recv(1):
-                    # 图像数据中存在错误，需要重传
-                    print("Data of img {} has error, need retransfer".format(img_name),end='\r')
-                else: 
-                    recv_success = True
-                    print('Check Complete',end='\r')
-                self.t_socket.send(cs)
-            img = base64.b64decode(b_img)
-            with open(task_dir+"/"+img_name, "wb") as img_file: 
-                img_file.write(img)
-                img_file.close
-            img_list.append([img_name,-1])
-        self.t_socket.close()
-        with open(task_dir+"/"+"csv.csv","w") as csv_file:
-            writer = csv.writer(csv_file)
-            writer.writerows(img_list)
-        self.task_list.append(client_id)
-    
-    def recv_task_with_json(self, max_seg_recv):
+    ''' def 从socket接收任务数据并写入磁盘 '''
+    def recv_task(self, max_seg_recv=0):
         self.connect()
         is_success = False
         n_timeout = 0
-        while not is_success:
-            if n_timeout > MAX_RETRANS:
-                return STAT_TIMEOUT
-            try:
-                size = int.from_bytes(self.t_socket.recv(4),byteorder="little")
-                task_pack = bytes()
-                recv_size = 0
-                recv_buff = bytes()
-                _MAX_SEG_RECV = size if max_seg_recv <= 0 else max_seg_recv
-                while recv_size < size:
-                    next_size = _MAX_SEG_RECV if size-recv_size > _MAX_SEG_RECV else size-recv_size
-                    recv_buff = self.t_socket.recv(next_size)
-                    recv_size += len(recv_buff)
-                    task_pack += recv_buff
-                    print("size: {}B | recv: {}B".format(size, recv_size),end='\r')
-                print('\nChecking...',end='\r')
-                cs = self.get_checksum(task_pack).to_bytes(1,"little")
-
-                # 异常测试
-                # cs = 0
-                # cs = cs.to_bytes(1,"little")
-                self.t_socket.send(cs)
-                if cs != self.t_socket.recv(1):
-                    print("Data Error, need to retransfer")
-                else:
-                    print("Checking Success. Transfer Complete")
-                    self.close()
-                    is_success = True
-            except socket.timeout:
-                print("Timeout, retry in {} time(s)".format(MAX_RETRANS-n_timeout))
-                n_timeout += 1
+        recv_stat, task_pack = tools.recv_data(self.t_socket)
+        # while not is_success:
+        #     if n_timeout > MAX_RETRANS:
+        #         self.close()
+        #         return STAT_TIMEOUT
+        #     try:
+        #         size = int.from_bytes(self.t_socket.recv(4),byteorder="little") # 报头后数据长度（包括校验和）
+        #         task_pack = bytes()
+        #         recv_size = 0
+        #         recv_buff = bytes()
+        #         _MAX_SEG_RECV = size if max_seg_recv <= 0 else max_seg_recv
+        #         while recv_size < size:
+        #             next_size = _MAX_SEG_RECV if size-recv_size > _MAX_SEG_RECV else size-recv_size
+        #             recv_buff = self.t_socket.recv(next_size)
+        #             recv_size += len(recv_buff)
+        #             task_pack += recv_buff
+        #             print("size: {}B | recv: {}B".format(size, recv_size),end='\r')
+        #         print('\nChecking...',end='\r')
+        #         cs = self.get_checksum(task_pack[:-1])
+        #         recv_cs = task_pack[-1]
+        
+        #         # 异常测试
+        #         # cs = 0
+        #         # cs = cs.to_bytes(1,"little")
+        #         l = 2
+        #         feedback = l.to_bytes(4,"little")+cs.to_bytes(1,"little")
+        #         feedback += tools.get_checksum(cs.to_bytes(1,"little")).to_bytes(1,"little")
+        #         self.t_socket.send(feedback)
+        #         # tools.send_data(self.t_socket,cs.to_bytes(1,"little"),check_method=None)
+        #         if cs != recv_cs:
+        #             print("Data Error, need to retransfer")
+        #         else:
+        #             print("Checking Success. Transfer Complete")
+        #             self.close()
+        #             is_success = True
+        #     except socket.timeout:
+        #         print("Timeout, retry in {} time(s)".format(MAX_RETRANS-n_timeout))
+        #         n_timeout += 1
         
         task_pack = json.loads(task_pack)
 
         # 创建任务目录
-        # os.mkdir(self.work_dir+"/"+client_id)
         task_dir = os.path.join(self.work_dir,task_pack["id"])
         os.mkdir(task_dir)
-        # task_dir = self.work_dir+client_id+"/"
 
         # 写入文件
         for idx in range(task_pack["n_imgs"]):
@@ -153,7 +97,6 @@ class TaskManager:
                 img_file.write(b_img)
                 img_file.close()
         
-        # client_id = task_pack["id"]
         file_list = []
         for file_name in task_pack["name_imgs"]:
             file_list.append([file_name, -1])
@@ -163,9 +106,6 @@ class TaskManager:
         self.task_list.append(task_pack["id"])
         
         return STAT_NOEEROR
-        
-
-
 
     def get_checksum(self, bstream):
         cs = 0 # checksum
@@ -174,8 +114,16 @@ class TaskManager:
         return cs
 
     ''' def 任务数据解包与任务包生成 '''
+    def get_task(self):
+        return self.task_list.pop(0) # 返回当前任务列表中最先提交任务的用户ID
 
     ''' def 接收预测结果并从socket回传 '''
+    def send_result(self, result):
+        self.connect()
+        feedback = {"msg": result}
+        bstream = json.dumps(feedback)
+
+        pass
 
 aliyun = "120.26.147.239"
 virtual = "192.168.109.131"
@@ -188,7 +136,7 @@ if __name__ == "__main__":
     while True:
         try:
             # tm.recv_task()
-            stat_flag = tm.recv_task_with_json(max_seg_recv=0)
+            stat_flag = tm.recv_task(max_seg_recv=0)
         except KeyboardInterrupt:
             tm.close()
             break
